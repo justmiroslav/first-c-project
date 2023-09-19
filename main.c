456fc210340670041166e0e6476dbd4532281d0c
justmiroslav <mirosmale@gmail.com>
2023-09-19 12:18:03 +0300

    4(upd) and 5 task

--- main.c
+++ main.c
@@ -37,11 +37,13 @@ void load_text(char *filename) {
     FILE *file = fopen(filename, "r");
     if (file != NULL) {
         char line[100];
+        text_len = 0;
         while (fgets(line, sizeof(line), file) != NULL) {
-            printf("%s", line);
+            strcat(text, line);
+            text_len += strlen(line);
         }
-        printf("\n");
         fclose(file);
+        printf("Text has been loaded successfully\n");
     } else {
         printf("Error opening file for loading\n");
     }
@@ -51,8 +53,40 @@ void print_text() {
     printf("%s\n", text);
 }
 
+void insert_text(int line, int index, char *new_text) {
+    if (line < 0 || line >= text_len) {
+        printf("Error: Invalid line\n");
+        return;
+    }
+    int new_len = strlen(new_text);
+    int current_line = 0;
+    int i = 0;
+    while (current_line < line) {
+        if (text[i] == '\n') {
+            current_line++;
+        }
+        i++;
+    }
+    int line_end = i;
+    while (text[line_end] != '\n' && text[line_end] != '\0') {
+        line_end++;
+    }
+    if (index < 0 || index > line_end || text_len + new_len >= 100) {
+        printf("Error: Invalid index or text too long\n");
+        return;
+    }
+    i += index;
+    for (int j = line_end + new_len; j > i; j--) {
+        text[j] = text[j - new_len];
+    }
+    for (int j = i, k = 0; k < new_len; j++, k++) {
+        text[j] = new_text[k];
+    }
+    text_len += new_len;
+}
+
 int main() {
-    int command;
+    int command, line, index;
     char input[100];
     char filename[100];
     while (true) {
@@ -75,6 +109,12 @@ int main() {
             load_text(filename);
         } else if (command == 5) {
             print_text();
+        } else if (command == 6) {
+            printf("Choose line and index:\n");
+            scanf("%d %d", &line, &index);
+            printf("Enter text to insert:\n");
+            scanf(" %[^\n]", input);
+            insert_text(line, index, input);
         } else if (command == 8) {
             break;
         } else {
